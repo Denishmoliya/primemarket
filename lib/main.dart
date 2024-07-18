c141112e6a6fc86b02387f544382aa6daa6f9da2
Denishmoliya <127723111+Denishmoliya@users.noreply.github.com>
2024-07-18 11:50:02 +0530

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Prime Market',
+      themeMode: ThemeMode.system,
       theme: ThemeData(
-
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(fontSize: 14,color: Colors.green)
+        ),
         primarySwatch: Colors.blue,
       ),
+      darkTheme: ThemeData(),
       debugShowCheckedModeBanner: false,
       home: const splashscreen(),
     );
