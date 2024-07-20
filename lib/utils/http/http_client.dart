47b264ba6feb3687cec212dfd0ecde69d35a2fff
Rajvi-pokiya <rajvipokiya889@gmail.com>
2024-07-20 18:17:08 +0530

    init commit

---
+++ lib/utils/http/http_client.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart'as http;
+
+class THttpHelper{
+  static const  String _baseUrl ='https://your-api-base-url.com';
+
+  static Future<Map<String,dynamic>> get(String endPoint)
+
+}
